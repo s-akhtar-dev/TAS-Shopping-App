72588781b79e18221147f82f5a7dd94b257169a3
Sarah Akhtar <s_akhtar@u.pacific.edu>
2024-10-18 19:14:54 -0700

    Initial Commit

---
+++ TASShopping/TASShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TASShoppingApp.swift
+//  TASShopping
+//
+//  Created by Sarah Akhtar on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TASShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
